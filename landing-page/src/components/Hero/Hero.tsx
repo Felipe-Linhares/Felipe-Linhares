2eb6afcf14118fe46c3582485f5b4a93a471071d
Felipe Linhares <70000056+Felipe-Linhares@users.noreply.github.com>
2025-06-04 02:50:48 -0300

    Update Hero.tsx

--- landing-page/src/components/Hero/Hero.tsx
+++ landing-page/src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
     <section id="hero" className="hero-section bg-light">
       <div className="container">
         <img
-          src="/assets/profile.jpg"
+          src="../../assets/profile.jpg"
           alt="Your Profile Picture"
           className="profile-pic"
         />
